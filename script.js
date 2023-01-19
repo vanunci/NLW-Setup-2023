2e0c983a5d7b1b3849ddf556a27c78ab48a80617
vanunci <mateushmvanunci@gmail.com>
2023-01-19 19:59:48 -0300

    fourth day to nlw setup event

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
-  water: ["01-04", "01-05"],
-  fruit: ["01-01", "01-03"],
-  journal: ["01-08", "01-09"],
-  medication: ["01-01", "01-02"],
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+function add() {
+
+  const today = new Date().toLocaleDateString('pt-br').slice(0, 5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso! ❌")
+    return
+  }
+
+  alert("Adicionado com sucesso ✔")
+  nlwSetup.addDay(today)
 }
+
+function save() {
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+
+  localStorage.getItem('NLWSetup@habits')
+  JSON.parse(localStorage.getItem('NLWSetup@habits'))
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
 
 nlwSetup.setData(data)
 nlwSetup.load()
